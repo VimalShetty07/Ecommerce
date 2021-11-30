3bc5f4ef16d57638a64291c9a6a729b2709ece89
vimal <shettyvimal07@gmail.com>
2021-11-30 21:15:37 +0530

    Major backend completed

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoutes");
 
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 
 // Middleware for Errors
