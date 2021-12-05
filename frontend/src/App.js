b9da11ccba9442d72d322c4a008a79ff8c2a1511
vimal <shettyvimal07@gmail.com>
2021-12-05 12:37:09 +0530

    Added all filters in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import React from "react";
 import Footer from "./components/layout/Footer/Footer";
 import Home from "./components/Home/Home"
 import ProductDetails from "./components/Product/ProductDetails"
+import Products from "./components/Product/Products"
+import Search from "./components/Product/Search"
 
 function App() {
 React.useEffect(() => {
@@ -20,6 +22,9 @@ React.useEffect(() => {
     <Header />
     <Route exact path="/" component={Home} />
     <Route exact path="/product/:id" component={ProductDetails} />
+    <Route exact path="/products" component={Products} />
+    <Route  path="/products/:keyword" component={Products} />
+    <Route exact path="/search" component={Search} />
     <Footer />
     </Router>
 
